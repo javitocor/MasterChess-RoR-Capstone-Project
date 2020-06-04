f1f8c8d89fc988b565fa370c2c196c3403c2ff83
javitocor <javitocor@gmail.com>
2020-06-04 15:02:14 +0200

    Modified right side on index and profile views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :fullname, :email, :password, :password_confirmation) }
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :fullname, :email, :password, :current_password) }
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:avatar, :cover_photo, :username, :fullname, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:avatar, :cover_photo, :username, :fullname, :email, :password, :current_password) }
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,7 @@ class User < ApplicationRecord
 
   has_many :friends, class_name: "Following", foreign_key: "follower_id"
   has_many :followers, class_name: "Following", foreign_key: "followed_id"
+
+  has_one_attached :avatar
+  has_one_attached :cover_photo
 end
