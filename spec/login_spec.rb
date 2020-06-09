88a08d02abe348e4756ecc50e88c0d6187014ceb
javitocor <javitocor@gmail.com>
2020-06-09 17:35:50 +0200

    Added error test for login and signup

--- spec/login_spec.rb
+++ spec/login_spec.rb
@@ -19,7 +19,21 @@ RSpec.describe 'Login and signup features', type: :feature do
       click_link 'Resign'
       expect(page).to have_content('Sign In')
     end
-  end
+    scenario 'validating email' do 
+      visit new_user_session_path
+      fill_in 'user_email', with: 'thing@.com'
+      fill_in 'user_password', with: user.password
+      click_button 'Log in'
+      expect(page).to have_content('Invalid Email or password.')
+    end
+    scenario 'validating password' do 
+      visit new_user_session_path
+      fill_in 'user_email', with: user.email
+      fill_in 'user_password', with: '56454'
+      click_button 'Log in'
+      expect(page).to have_content('Invalid Email or password.')
+    end
+  end 
   context 'signup' do
     scenario 'signup' do
       visit new_user_registration_path
@@ -30,6 +44,56 @@ RSpec.describe 'Login and signup features', type: :feature do
       fill_in 'user_password_confirmation', with: 'password'
       click_button 'Sign Up'
       expect(page).to have_content('Welcome!')
+    end
+    scenario 'fail signup' do 
+      visit new_user_registration_path
+      click_button 'Sign Up'
+      expect(page).to have_content('Please review the problems below:')
+    end
+    scenario 'username validation' do 
+      visit new_user_registration_path
+      fill_in 'user_fullname', with: 'Peter Grif'
+      fill_in 'user_email', with: 'peter@example.com'
+      fill_in 'user_password', with: 'password'
+      fill_in 'user_password_confirmation', with: 'password'
+      click_button 'Sign Up'
+      expect(page).to have_content("Username can't be blank")
+    end
+    scenario 'fullname validation' do 
+      visit new_user_registration_path
+      fill_in 'user_username', with: 'Peter'
+      fill_in 'user_email', with: 'peter@example.com'
+      fill_in 'user_password', with: 'password'
+      fill_in 'user_password_confirmation', with: 'password'
+      click_button 'Sign Up'
+      expect(page).to have_content("Fullname can't be blank")
+    end
+    scenario 'email validation' do 
+      visit new_user_registration_path
+      fill_in 'user_username', with: 'Peter'
+      fill_in 'user_fullname', with: 'Peter Grif'
+      fill_in 'user_password', with: 'password'
+      fill_in 'user_password_confirmation', with: 'password'
+      click_button 'Sign Up'
+      expect(page).to have_content("Email can't be blank")
+    end
+    scenario 'password validation' do 
+      visit new_user_registration_path
+      fill_in 'user_username', with: 'Peter'
+      fill_in 'user_fullname', with: 'Peter Grif'
+      fill_in 'user_email', with: 'peter@example.com'
+      click_button 'Sign Up'
+      expect(page).to have_content("Password can't be blank")
+    end
+    scenario 'Valid email' do 
+      visit new_user_registration_path
+      fill_in 'user_username', with: 'Peter'
+      fill_in 'user_fullname', with: 'Peter Grif'
+      fill_in 'user_email', with: 'peterexample.com'
+      fill_in 'user_password', with: 'password'
+      fill_in 'user_password_confirmation', with: 'password'
+      click_button 'Sign Up'
+      expect(page).to have_content('Email is invalid')
     end
   end
   describe 'fields in access pages' do
